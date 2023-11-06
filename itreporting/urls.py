2900d7a0f1a862682a7887760da13e9c5a616ebd
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-06 13:19:22 +0000

    fixed template issue

--- itreporting/urls.py
+++ itreporting/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users import views as users_views
+from users import views as user_views
 from . import views 
 
 app_name = 'itreporting'
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
-    path('register', users_views.register, name = 'register'),
+    path('register', user_views.register, name='register'),
 ]
