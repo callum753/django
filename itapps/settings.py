fd60aefbdb40755b798a1c44cddcedb80fc027df
callum753 <c0013851@hallam.shu.ac.uk>
2023-11-10 13:06:45 +0000

    update to make profiles display pictures

--- itapps/settings.py
+++ itapps/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -130,3 +132,4 @@ CRISPY_ALLOWED_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'itreporting:home'
+LOGIN_URL = 'login'
--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,21 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(label = 'Email address', help_text = 'your SHU email adress')
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email']
 
 class Meta:
     model = User
